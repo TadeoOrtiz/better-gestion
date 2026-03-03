e75e418118696f690999f3b7f7c8e5207393c523
MioKuguisaki <tadeolisto5@gmail.com>
2026-03-03 12:08:21 -0300

    ASD

--- frontend/src/components/ConfigPanel.jsx
+++ frontend/src/components/ConfigPanel.jsx
@@ -2,10 +2,8 @@ import React, { useState } from "react";
 
 export default function ConfigPanel({ onDataReceived }) {
     const [formData, setFormData] = useState({
-        nombre: "Alex Drift",
-        email: "alex.test@example.com",
-        password: "password123",
-        sector: "Tecnología",
+        name: "",
+        age: 0,
     });
 
     // 2. Manejador para actualizar el estado cuando el usuario escribe
@@ -30,62 +28,30 @@ export default function ConfigPanel({ onDataReceived }) {
 
     return (
         <div className="max-w-sm mx-auto">
-            <h2>Registro de Usuario</h2>
+            <h2>Registrar materia</h2>
             <form onSubmit={handleSubmit}>
-                <div className="relative z-0 w-full mb-5 group">
+                <div className="relative z-0 w-full mb-5 group gap-5 flex">
                     <input
-                        className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
+                        className=" w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
                         type="text"
                         name="nombre"
                         placeholder="Nombre"
-                        value={formData.nombre}
+                        value={formData.name}
                         onChange={handleChange}
-                        style={{ width: "100%" }}
                     />
 
                     <input
-                        className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
-                        type="email"
-                        name="email"
+                        className="w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
+                        type="number"
+                        name="age"
                         placeholder="email@email.com"
-                        value={formData.email}
+                        value={formData.age}
                         onChange={handleChange}
-                        style={{ width: "100%" }}
                     />
-
-                    <input
-                        className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
-                        placeholder="password"
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        style={{ width: "100%" }}
-                    />
-
-                    <div class="flex items-center mb-4">
-                        <input
-                            checked
-                            id="checkbox-1"
-                            type="checkbox"
-                            value=""
-                            className="w-4 h-4 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft"
-                        />
-                        <label
-                            for="checkbox-1"
-                            class="ms-2 text-sm font-medium text-heading select-none"
-                        >
-                            I agree to the{" "}
-                            <a href="#" class="text-fg-brand hover:underline">
-                                terms and conditions
-                            </a>
-                            .
-                        </label>
-                    </div>
 
                     <button
                         type="submit"
-                        className="text-white bg-blue-500 box-border border border-transparent hover:bg-blue-400 focus:ring-2 focus:ring-blue-300 shadow-xs font-medium rounded-xl text-sm px-4 py-2.5 focus:outline-none"
+                        className="text-white bg-blue-500 box-border border border-transparent hover:bg-blue-400 focus:ring-2 focus:ring-blue-300 shadow-xs font-medium rounded-xl text-sm px-5 focus:outline-none"
                     >
                         Enviar Datos
                     </button>
